74a7a00ecbca442fb7bbe9528aa7d89cf32e67b4
Your Name <you@example.com>
2026-01-08 05:10:00 +0000

    Fix: Atualizando link e index.html

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// Atualizado automaticamente em qui 08 jan 2026 05:02:58 UTC
-console.log('? Redirecionando para o servidor local: http://bore.pub:16504');
-window.location.href = 'http://bore.pub:16504';
+// Atualizado em qui 08 jan 2026 05:10:00 UTC
+console.log('? Redirecionando para: http://bore.pub:63496');
+window.location.href = 'http://bore.pub:63496';
