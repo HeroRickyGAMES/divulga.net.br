e7f23b81aa48d91852549b785f46c264c3500144
Your Name <you@example.com>
2026-01-08 05:02:58 +0000

    update: nova url http://bore.pub:16504

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+// Atualizado automaticamente em qui 08 jan 2026 05:02:58 UTC
+console.log('? Redirecionando para o servidor local: http://bore.pub:16504');
+window.location.href = 'http://bore.pub:16504';
